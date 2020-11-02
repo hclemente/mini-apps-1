548b9bdd5cd0fb6d5210aa9b9d124a66d6fdcc3e
Harry Clemente <hclemente1989@gmail.com>
2020-11-02 11:22:02 -0800

    To Do list started with first 3 problem solving steps

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,25 @@
-
+/**
+ * To Do
+ *
+ * Problem Solving Process
+ *  Justification:
+ *    Build a single-page app that takes a user through a series of forms that
+ *    simulate a shopping cart checkout experience
+ *  Specification:
+ *    Multiple text boxes and buttons
+ *  Explanation:
+ *    We will walk the user through a check out expexrience. Multiple forms will
+ *    be completed which will be submitted to the server and saved in a database.
+ *    The forms will contain information about the shopper and the items they are
+ *    purchasing such as the shoppers name, and billing and shipping addresses, and
+ *    payment information. The items being purchased will be visible, along with
+ *    their respective quantities being purchased.
+ *  Visualization:
+ *  Approximation
+ *  Verification:
+ *  Implementation
+ * Basic File setup
+ * Figure out what Libraries I need
+ *
+ */
 
