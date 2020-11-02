6b3925a19f95f2d07e32630395ef835f8595d7a7
Harry Clemente <hclemente1989@gmail.com>
2020-11-02 11:06:43 -0800

    To Do list started and file setup for html

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,24 @@
-
+/**
+ * To Do
+ *
+ * Make a Tic Tac Toe game with the following requirements
+ *
+ *  * the first move always starts with player X
+ *  * the app detects a win or tie and displays an appropriate message
+ *  * a button resets the game for a new round of gameplay
+ *
+ * App.js To Do
+ *
+ * Probelm Solving Process
+ * Basic structural setup
+ * find out what libraries are allowed and will be needed
+ *
+ *
+ * index.html To Do
+ *
+ * Probelm Solving Process
+ * Basic structural setup
+ * find out what libraries are allowed and will be needed
+ *
+ */
 
